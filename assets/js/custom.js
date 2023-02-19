b6ed0c463d2f1b341788d82f062f6ef04d9a9b5b
Onup Chandra Barmon <43741354+onuproy@users.noreply.github.com>
2023-02-19 17:34:52 +0600

    UDpate html old file

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function(){ 
+
+	// Mobile menu
+	$('.nav-icon').click(function () {
+		$('.mobile_menu').toggleClass('canvas-menu');
+		return false;
+
+	});
+	$('.mobile_menu ul li a').click(function () {
+		$('.mobile_menu').toggleClass('canvas-menu');
+
+	});
+
+	
+
+		
+		
+	
+
+});
